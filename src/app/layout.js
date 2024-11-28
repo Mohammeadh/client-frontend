78815e41f7c4b69a3831404defe9e0be6aadd898
Mohammeadh <130226741+Mohammeadh@users.noreply.github.com>
2024-11-28 16:57:55 +0530

    layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,6 +7,7 @@ export default function Layout({ children }) {
   return (
     <html lang="en">
       <head>
+    <link rel="icon" href="/favicon.ico" />
       <link
   href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
   rel="stylesheet"
