ce69b422b1c85164077da10902baa94462bd53ba
Mohammeadh <130226741+Mohammeadh@users.noreply.github.com>
2024-11-28 19:03:21 +0530

    SocketContext.js

--- src/context/SocketContext.js
+++ src/context/SocketContext.js
@@ -8,7 +8,7 @@ import '../app/globals.css';
 const SocketContext = createContext();
 
 const socket = io('https://video-chat-mohammed.vercel.app', {
-  transports: ['polling', 'websocket'], // Use both polling and websocket
+  transports: ['websocket'], // Use both polling and websocket
   withCredentials: true,
 });
 
